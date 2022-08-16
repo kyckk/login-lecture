b60e6534fb25897dba2c7af7c2ba79df1b59d799
someone <someone@someplace.com>
2022-08-16 15:51:20 +0900

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -22,5 +22,16 @@
     
         return newUsers;
      }
+
+     static getUserInfo(id){
+        const users= this.#users;
+        const idx =users.id.indexOf(id);
+        const usersKeys=Object.keys(users);//=>[id,psword]
+        const userInfo =usersKeys.reduce((newUser,info)=>{
+            newUser[info] =users[info][idx];
+            return newUser;
+        },{});
+        return userInfo;
+     }
 }
 module.exports = UserStorage;
