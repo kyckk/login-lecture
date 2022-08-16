d1557b5105cea4e39d605c2b23bd157cd2d443df
someone <someone@someplace.com>
2022-08-16 16:22:06 +0900

    회원가입페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -9,5 +9,6 @@ router.get("/", ctrl.output.home);
 //router= 단순히 클라이언트가 도메인으로 요청이 들어왓을때 연결해주는애
 //구현부가 컨트롤러역할을 수행해준다 
 router.get("/login", ctrl.output.login);
+router.get("/register",ctrl.output.register);
 router.post("/login", ctrl.process.login);
 module.exports = router;
