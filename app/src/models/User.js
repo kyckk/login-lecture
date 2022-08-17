ba7d243345121a27447fa3f3c38546e5c0319a82
someone <someone@someplace.com>
2022-08-17 17:46:20 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,11 +7,12 @@ class User{
 
     }
 
-    login(){
+   async login(){
+        
         
         const client =this.body;
-        const {id, psword}= UserStorage.getUserInfo(client.id);
-
+        const {id,psword}=await UserStorage.getUserInfo(client.id);//getUserInfo가 수행시간이느려서 (promise를반환할경우) await를붙여서 기다려준다
+                                                                //따라서 login자체도 느려질수밖에없다그래서 컨트롤러로가서 await을 붙여준다
         if(id){
             if(id === client.id && psword === client.psword){
                 return{ success:true};
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,21 @@
 "use strict";
 
+    const fs = require("fs").promises;    
+
     class UserStorage {
-      static #users ={//내부데이터는 변수명으로 바로호출되면안된다=>은닉화를 '#변수명'
-            id:["woorimIT","나개발","강팀장"],
-            psword:["1234","1234","123456"],
-            name:["우리밋", "나개발","김팀장"],
-        };
+        static #getUserInfo(data, id){
+            const users =JSON.parse(data);
+                const idx =users.id.indexOf(id);
+                const usersKeys=Object.keys(users);//=>[id,psword]
+                const userInfo =usersKeys.reduce((newUser,info)=>{
+                    newUser[info] =users[info][idx];
+                    return newUser;
+                },{});
+                return userInfo;
+         }
      static getUsers(...fields){
         
-        const users =this.#users;
+       // const users =this.#users;
         //console.log(...fields);
         //console.log(users);
         const newUsers =fields.reduce((newUsers,field)=>{
@@ -24,17 +31,24 @@
      }
 
      static getUserInfo(id){
-        const users= this.#users;
-        const idx =users.id.indexOf(id);
-        const usersKeys=Object.keys(users);//=>[id,psword]
-        const userInfo =usersKeys.reduce((newUser,info)=>{
-            newUser[info] =users[info][idx];
-            return newUser;
-        },{});
-        return userInfo;
+        //pending이랑 데이터를 전부읽어오지 못햇다라는뜻이다
+        //const users= this.#users;
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=>{
+            
+           return this.#getUserInfo(data,id);
+
+        })//then은 위해당로직이 성공하면실행된다
+        .catch((err)=>console.error(err));//위해당로직이 실패하면실행된다(err)=>console.error(err)=console.error
+
+        
+     
+       
      }
+     
      static save(userInfo){
-        const users =this.#users;
+       // const users =this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
