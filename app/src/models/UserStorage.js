f4e4c5d19c836bd4c56e75be50f9fc2f7c009247
someone <someone@someplace.com>
2022-08-18 14:31:59 +0900

    fs모듈을 통한 회원가입 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -13,11 +13,9 @@
                 },{});
                 return userInfo;
          }
-     static getUsers(...fields){
-        
-       // const users =this.#users;
-        //console.log(...fields);
-        //console.log(users);
+     static #getUsers(data,isAll, fields){
+        const users =JSON.parse(data);
+        if(isAll) return users
         const newUsers =fields.reduce((newUsers,field)=>{
            
             if(users.hasOwnProperty(field)){
@@ -28,6 +26,22 @@
         },{});
     
         return newUsers;
+     }
+     static getUsers(isAll,...fields){
+        
+       // const users =this.#users;
+        //console.log(...fields);
+        //console.log(users);
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=>{
+            
+           return this.#getUsers(data,isAll,fields);
+
+        })//then은 위해당로직이 성공하면실행된다
+        .catch((err)=>console.error(err));
+
+        
      }
 
      static getUserInfo(id){
@@ -47,12 +61,18 @@
        
      }
      
-     static save(userInfo){
+     static async save(userInfo){
        // const users =this.#users;
-        users.id.push(userInfo.id);
-        users.name.push(userInfo.name);
-        users.psword.push(userInfo.psword);
-        return { success: true};
+       const users =await this.getUsers(true);//모든데이터를 가지고오겠다->"id", "psword", "name"대신인자를 true로쓴다
+       console.log(users);
+       if(users.id.includes(userInfo.id)){
+        throw "이미 존재하는 아이디입니다.";
+       }
+       users.id.push(userInfo.id);
+       users.name.push(userInfo.name);
+       users.psword.push(userInfo.psword);
+       fs.writeFile("./src/databases/users.json", JSON.stringify(users));
+       return{success: true};
      }
 }
 module.exports = UserStorage;
